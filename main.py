6ab7bdac135bafe266298c047453ac222791fdb2
JoseT20 <56453956+JoseT20@users.noreply.github.com>
2022-03-18 00:41:42 -0700

    Add feature to fetch new anime episodes and their data

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from datetime import date
-import requests
 import time
 
 url = 'https://www.crunchyroll.com/es-es/simulcastcalendar'
@@ -9,6 +8,19 @@ driver = webdriver.Chrome('./chromedriver')
 driver.get(url)
 source_data = driver.page_source
 soup = BeautifulSoup(source_data, 'lxml')
-new_episodes = soup.find('li', class_='day today active')
-print(new_episodes)
+today_data = soup.find('li', class_='day active today')
+new_episodes = today_data.find_all('li')
+
+for new_episode in new_episodes:
+    season_name = new_episode.find('h1', class_='season-name').text.strip()
+    episode = new_episode.find('div', class_='episode-label').text.strip()
+    release_date = today_data.find('div', class_='specific-date').text.strip()
+    release_time = new_episode.find('time', class_='available-time').text.strip()
+    link = new_episode.find('div', class_='availability').a['href']
+
+    print(f'Anime: {season_name}')
+    print(f'Episode: {episode}')
+    print(f'Release Date: {release_date} at {release_time}')
+    print(f'Link: {link} \n')
+
 
